50ae798bec100f8ceeceb1adf3bcfe9b049375fe
Jack Royal <jroyal@oucs1619.otago.ac.nz>
2019-09-30 10:00:33 +1300

    Updated permissions is definetlyoverkill but it works, if have time later will update to be more realistic

--- querysitescript.sh
+++ querysitescript.sh
@@ -6,6 +6,13 @@ apt-get install -y apache2 php libapache2-mod-php php-mysql
 # (Look inside query-website.conf for specifics.)
 cp /vagrant/query-site.conf /etc/apache2/sites-available/
 
+chmod 755 /vagrant
+chmod 755 /vagrant/query
+chmod 755 /home/ubuntu
+
+cd /vagrant/query
+find . -type f -exec chmod 644 {} \;
+
 a2ensite query-site
 a2dissite 000-default
 service apache2 reload
