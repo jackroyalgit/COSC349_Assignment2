5a5a240df418cda9a9695ee1d33e4f0745d569b9
Jack Royal <jroyal@oucs1619.otago.ac.nz>
2019-09-30 10:44:48 +1300

    Using code from assignment 1 query the database only change is how i connect to the DB

--- query/index.php
+++ query/index.php
@@ -1,8 +1,42 @@
 <html>
-<head>
-<title>PHP Test</title>
-</head>
-<body>
-<?php echo '<p>Hello World</p>'; ?> 
+  <link rel="stylesheet"type="text/css" href="style.css">
+  <div id="main">
+    <head>
+    </head>
+    <body>
+      <h1>Showing contents of websiteuserusers table:</h1>
+      <table border="1">
+        <tr><th>First Name</th><th>Last Name</th><th>Occupation</th><th>Fav Food</th><th>Fav Music</th><th>Fav Sport</th></tr>
+  </div>
+  <?php
+     $db_host   = 'database-349.csyjl0hvsn6y.us-east-1.rds.amazonaws.com';
+     $db_name   = 'sample';
+     $db_user   = 'assignment_user';
+     $db_passwd = 'lilodog1';
+     
+     $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+     
+     $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+     $q = $pdo->query("SELECT * FROM websiteuser");
+  
+  while($row = $q->fetch()){
+  echo "<tr><td>".$row["fname"]."</td><td>".$row["lname"]."</td><td>".$row["occupation"]."</td><td>".$row["food"]."</td><td>".$row["music"]."</td><td>".$row["sport"]."</td></tr>\n";
+  }
+  ?>
+  </table>
+
+  <h1>Showing first, last names of websiteuserusers table:</h1>
+  <table border="1">
+    <tr><th>First Name</th><th>Last Name</th></tr>
+    <?php
+       $q1 = $pdo->query("SELECT fname,lname FROM websiteuser");
+    while($row = $q1->fetch()){
+    echo "<tr><td>".$row["fname"]."</td><td>".$row["lname"]."</td></tr>\n";
+    }
+    ?>
+  </table>
+
+  <p>Hello would you like to input some <a href="http://192.168.33.10"> data</a>?</p>
 </body>
 </html>
+
---
+++ testdb.sql
@@ -0,0 +1,17 @@
+CREATE TABLE websiteuser (
+fname varchar(20) NOT NULL,
+lname varchar(20) NOT NULL,
+occupation varchar(20) NOT NULL,
+food varchar(20) NOT NULL,
+music varchar(20) NOT NULL,
+sport varchar(20) NOT NULL
+);
+
+INSERT INTO websiteuser VALUES ('Jack','Royal','Student','Chicken','Rock','Football');
+INSERT INTO websiteuser VALUES ('John','Andrews','Lawyer','Beef','Pop','Rugby');
+INSERT INTO websiteuser VALUES ('Bill','Royal','Architect','Chicken Soup','Classical','AFL');
+INSERT INTO websiteuser VALUES ('Joe','Bndrews','Rubbish Man','Pie','Jazz','Bowls');
+INSERT INTO websiteuser VALUES ('Samson','Lambelle','Student','Chips','House','Skiing');
+INSERT INTO websiteuser VALUES ('Sally','Bossor','Litigator','Cake','Rap','Netball');
+INSERT INTO websiteuser VALUES ('Bob','Marley','Pastor','Brownie','Reggae','Cricket');
+INSERT INTO websiteuser VALUES ('Jack','Johnson','Musician','Pineapple','Folk','Badminton');
