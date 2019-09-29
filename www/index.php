bb41045fda630c3ce13bec0d10c01e5d6af56b5c
Jack Royal <jroyal@oucs1619.otago.ac.nz>
2019-09-30 11:05:54 +1300

    Website now functions identically to how it does in Assignment1

--- www/index.php
+++ www/index.php
@@ -1,14 +1,31 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
 <html>
   <link rel="stylesheet"type="text/css" href="style.css">
-  <div id="main">
-  <head>
-  </head>
   <body>
-    <h1>Showing contents of websiteuserusers table:</h1>
-    <table border="1">
-      <tr><th>First Name</th><th>Last Name</th><th>Occupation</th><th>Fav Food</th><th>Fav Music</th><th>Fav Sport</th></tr>
-  </div>
+    <div id="main">
+      <h1>Company X - User Survey</h1>
+      <div id="websiteuserinput">
+        <h2>Company Employee</h2>
+        <hr/>
+        <form action="" method="post">
+          <label>First Name :</label>
+          <input type="text" name="fname" id="fname" required="required" placeholder="Please Enter First Name"/><br /><br />
+          <label>Last Name :</label>
+          <input type="text" name="lname" id="lname" required="required" placeholder="Please Enter Second Name"/><br/><br />
+          <label>Occupation :</label>
+          <input type="text" name="occupation" id="occupation" required="required" placeholder="Please Enter Your Occupation"/><br/><br />
+          <label>Favourite Food :</label>
+          <input type="text" name="food" id="food" required="required" placeholder="Favourite Food?"/><br/><br />
+          <label>Favourite Music :</label>
+          <input type="text" name="music" id="music" required="required" placeholder="Favourite Music?"/><br/><br />
+          <label>Favourite Sport :</label>
+          <input type="text" name="sport" id="sport" required="required" placeholder="Favourite Sport?"/><br/><br />
+          <input type="submit" value=" Submit " name="submit"/><br />
+        </form>
+      </div>
+
       <?php
+         if(isset($_POST["submit"])){ 
          $db_host   = 'database-349.csyjl0hvsn6y.us-east-1.rds.amazonaws.com';
          $db_name   = 'sample';
          $db_user   = 'assignment_user';
@@ -17,27 +34,22 @@
          $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
 
          $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
-         $q = $pdo->query("SELECT * FROM websiteuser");
 
-      while($row = $q->fetch()){
-      echo "<tr><td>".$row["fname"]."</td><td>".$row["lname"]."</td><td>".$row["occupation"]."</td><td>".$row["food"]."</td><td>".$row["music"]."</td><td>".$row["sport"]."</td></tr>\n";
+         $sql = "INSERT INTO websiteuser (fname, lname, occupation, food, music, sport)
+                 VALUES ('".$_POST["fname"]."','".$_POST["lname"]."','".$_POST["occupation"]."','".$_POST["food"]."','".$_POST["music"]."','".$_POST["sport"]."')";
+
+         if ($pdo->query($sql)) {
+      echo "<script type= 'text/javascript'>alert('New User Inserted Succesfully');</script>";
       }
+
+      else{
+      echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+      }
+      }
+
       ?>
-  </table>
-
-  <h1>Showing first, last names of websiteuserusers table:</h1>
-  <table border="1">
-    <tr><th>First Name</th><th>Last Name</th></tr>
-   <?php
-      $q1 = $pdo->query("SELECT fname,lname FROM websiteuser");
-      while($row = $q1->fetch()){
-      echo "<tr><td>".$row["fname"]."</td><td>".$row["lname"]."</td></tr>\n";
-    }
-    ?>
-  </table>
-   
-    <p>Hello would you like to input some <a href="http://192.168.33.10"> data</a>?</p>
-  </body>
+</table>
+<p>Hello. Do you want to view the <a href="http://192.168.33.12"> results from the survey</a>?</p>
+</div>
+</body>
 </html>
-
-
